06c70308700c3dda1d07fb1f8307879653ba6e53
Glenn Hollingsworth <duelgamer@gmail.com>
2015-01-21 13:42:01 -0500

    First test at filling in

---
+++ timedoc.js
@@ -0,0 +1,8 @@
+function fillIn(username, enddate, day1, day2, day3, day4, hours) {
+    document.getElementById('username').innerHTML = username;
+    document.getElementById('enddate').innerHTML = enddate;
+    document.getElementById('day1').innerHTML = day1;
+    document.getElementById('day2').innerHTML = day2;
+    document.getElementById('day3').innerHTML = day3;
+    document.getElementById('day4').innerHTML = day4;
+}
